501e820d51992b620b6ec0d32254061272199ef6
Trus Sangwarn <trus.sangwarn@icloud.com>
2016-10-14 16:37:27 +0700

    debounce

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyDdQ710sR7m1UR3EK3HajxHUySEmAHN2LU';
 
 // Create a new component. This component should produce some HTML
@@ -29,9 +30,10 @@ class App extends Component {
   };
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
